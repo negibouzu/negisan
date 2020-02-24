b2a625a560f71daf6df4dfd74b0aea3e4590e0bb
negibouzu <60998393+negibouzu@users.noreply.github.com>
2020-02-24 11:41:20 +0900

    Create library.cpp

---
+++ library.cpp
@@ -0,0 +1,7 @@
+//chmin
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+
+//chmax
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
